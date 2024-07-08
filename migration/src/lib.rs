7c35ee7ea0060957c637c4927e4e937e59afe89f
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-07-09 00:03:14 +0800

    feat: add create profile table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20240708_142859_create_user_table;
+mod m20240708_155447_create_profile_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20240708_142859_create_user_table::Migration)]
+        vec![
+            Box::new(m20240708_142859_create_user_table::Migration),
+            Box::new(m20240708_155447_create_profile_table::Migration),
+        ]
     }
 }
---
+++ migration/src/m20240708_155447_create_profile_table.rs
@@ -0,0 +1,44 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Profile::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Profile::Id)
+                            .string_len(32)
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Profile::Name).string().not_null())
+                    .col(ColumnDef::new(Profile::Model).integer().not_null())
+                    .col(ColumnDef::new(Profile::SkinTexture).string().null())
+                    .col(ColumnDef::new(Profile::CapeTexture).string().null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Profile::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Profile {
+    Table,
+    Id,
+    Name,
+    Model,
+    SkinTexture,
+    CapeTexture,
+}
