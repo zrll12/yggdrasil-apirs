5aab2e24c1cb7f91629c525a0479cbb45672600c
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-08-05 21:20:05 +0800

    fix: username is null causing panic

--- src/model/generated/user.rs
+++ src/model/generated/user.rs
@@ -8,7 +8,8 @@ pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: String,
     #[sea_orm(unique)]
-    pub username: String,
+    pub username: Option<String>,
+    #[sea_orm(unique)]
     pub email: String,
     pub password: String,
     pub profile_id: String,
